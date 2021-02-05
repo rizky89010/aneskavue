959b78be9d809b2699105122fa236583a8d8b4b8
Drizky89010 <muhammadrizky89019@gmail.com>
2021-02-05 17:11:27 +0700

    upload project vue

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <HeaderAneska />
+    <carouselaneska />
+    <carouselbawah />
+    <instaaneska />
+    <partnerlogo />
+    <footeraneska/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderAneska from '@/components/HeaderAneska.vue';
+import carouselaneska from '@/components/carouselaneska.vue';
+import carouselbawah from '@/components/carouselbawah.vue';
+import instaaneska from '@/components/instaaneska.vue';
+import partnerlogo from '@/components/partnerlogo.vue';
+import footeraneska from '@/components/footeraneska.vue';
+
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderAneska,
+    carouselaneska,
+    carouselbawah,
+    instaaneska,
+    partnerlogo,
+    footeraneska,
+  }
+}
+</script>
